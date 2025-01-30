1ceba1dabd069611e7418cd8980fe417be9b0192
Sidharth <sidharth2020sharma@gmail.com>
2025-01-30 19:52:02 +0530

    Our Policy component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/Bestseller'
+import OurPolicy from '../components/OurPolicy'
 
 function Home() {
   return (
     <div>
       <Hero/> 
       <LatestCollection/> 
-      <BestSeller/>
+      <BestSeller/> 
+      <OurPolicy/>
     </div>
   )
 }
