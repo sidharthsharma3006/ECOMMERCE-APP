f45a45436ea026ab8663013661f505de7a6da96f
Sidharth <sidharth2020sharma@gmail.com>
2025-02-08 11:09:40 +0530

    Setup Category Filter Logic

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -9,10 +9,44 @@ function Collection() {
   const {products} = useContext(ShopContext);  
   const [showFilter,setShowFilter] = useState(false)
   const [filterProducts,setFilterProducts] = useState([]); 
+  const [category,setCategory] = useState([]); 
+  const [subCategory,setSubCategory] = useState([]); 
+
+  const toggleCategory = (e) => {
+    if(category.includes(e.target.value)){
+      setCategory(prev=>prev.filter(item=> item!== e.target.value ))
+    } 
+    else{
+      setCategory(prev => [...prev,e.target.value])
+    }
+  } 
+
+  const toggleSubCategory = (e) => {
+    if (subCategory.includes(e.target.value)){
+      setSubCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } 
+    else{
+      setSubCategory((prev) => [...prev, e.target.value]);
+    }
+  } 
+
+  const applyFilter = ()=>{
+    let productsCopy  = products.slice(); 
+    
+    if(category.length > 0){
+      productsCopy = productsCopy.filter(item => category.includes(item.category)); 
+    }  
+
+    if(subCategory.length > 0){
+      productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory))
+    }
+    setFilterProducts(productsCopy) 
   
+  }
+
   useEffect(()=>{
-    setFilterProducts(products)
-  },[])
+    applyFilter(); 
+  },[category,subCategory])
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
@@ -39,15 +73,31 @@ function Collection() {
           <p className="mb-3 text-sm font-medium">CATEGORIES</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className="flex gap-2">
-              <input type="checkbox" className="w-3" value={"Men"} />
+              <input
+                type="checkbox"
+                className="w-3"
+                value={"Men"}
+                onChange={toggleCategory}
+              />
               Men
             </p>
             <p className="flex gap-2">
-              <input type="checkbox" className="w-3" value={"Women"} />
+              <input
+                type="checkbox"
+                className="w-3"
+                value={"Women"}
+                onChange={toggleCategory}
+              />
               Women
             </p>
             <p className="flex gap-2">
-              <input type="checkbox" className="w-3" value={"Kids"} /> Kids
+              <input
+                type="checkbox"
+                className="w-3"
+                value={"Kids"}
+                onChange={toggleCategory}
+              />{" "}
+              Kids
             </p>
           </div>
         </div>
@@ -61,15 +111,30 @@ function Collection() {
           <p className="mb-3 text-sm font-medium">TYPE</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className="flex gap-2">
-              <input type="checkbox" className="w-3" value={"Topwear"} />
+              <input
+                type="checkbox"
+                className="w-3"
+                value={"Topwear"}
+                onChange={toggleSubCategory}
+              />
               Topwear
             </p>
             <p className="flex gap-2">
-              <input type="checkbox" className="w-3" value={"Bottomwear"} />
+              <input
+                type="checkbox"
+                className="w-3"
+                value={"Bottomwear"}
+                onChange={toggleSubCategory}
+              />
               Bottomwear
             </p>
             <p className="flex gap-2">
-              <input type="checkbox" className="w-3" value={"Winterwear"} />{" "}
+              <input
+                type="checkbox"
+                className="w-3"
+                value={"Winterwear"}
+                onChange={toggleSubCategory}
+              />{" "}
               Winterwear
             </p>
           </div>
@@ -93,14 +158,13 @@ function Collection() {
           {filterProducts.map((item, index) => (
             <ProductItem
               key={index}
-              name={item.name} 
+              name={item.name}
               price={item.price}
               id={item._id}
               image={item.image}
             />
           ))}
-        </div> 
-
+        </div>
       </div>
     </div>
   );
