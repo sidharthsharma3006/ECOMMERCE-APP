703c81d3721f1b8591bd40676288f8a0ffecdd87
Sidharth <sidharth2020sharma@gmail.com>
2025-02-08 17:34:37 +0530

    Implement SearchBar

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/Productitem';
 
 function Collection() { 
 
-  const {products} = useContext(ShopContext);  
+  const {products, search, showSearch } = useContext(ShopContext);  
   const [showFilter,setShowFilter] = useState(false)
   const [filterProducts,setFilterProducts] = useState([]); 
   const [category,setCategory] = useState([]); 
@@ -32,7 +32,12 @@ function Collection() {
   } 
 
   const applyFilter = ()=>{
-    let productsCopy  = products.slice(); 
+    let productsCopy  = products.slice();  
+
+
+    if(showSearch && search){
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
     
     if(category.length > 0){
       productsCopy = productsCopy.filter(item => category.includes(item.category)); 
@@ -67,7 +72,7 @@ function Collection() {
 
   useEffect(()=>{
     applyFilter(); 
-  },[category,subCategory]) 
+  },[category,subCategory,search,showSearch]) 
 
   useEffect(()=>{
     sortProduct(); 
