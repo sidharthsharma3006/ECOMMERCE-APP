906e5adaae768dd59a93e49f7bc1cdec121e68cf
Sidharth <sidharth2020sharma@gmail.com>
2025-03-02 18:40:42 +0530

    Create UserModels and UserRoutes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config' 
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
 
 
 //app config 
@@ -15,6 +16,10 @@ connectCloudinary()
 //middlewares 
 app.use(express.json()) 
 app.use(cors()) 
+
+//api endpoint 
+app.use('/api/user',userRouter) 
+
 
 //api endpoints 
 app.get('/',(req,res)=>{
